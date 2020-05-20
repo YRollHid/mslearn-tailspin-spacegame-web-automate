471fabfc743d759ac064d2c092d5062cd0276876
yansr <yans.roll@outlook.com>
2020-05-20 10:55:43 +0200

    Add provisioning job to the Dev stage

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
 terraform {
   required_version = "> 0.12.0"
 
